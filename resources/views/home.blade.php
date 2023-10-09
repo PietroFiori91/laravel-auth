c8a3ed0a9a4805ebc260db111b19c1c0464267bb
PietroFiori91 <pederflowers@hotmail.it>
2023-10-09 18:04:50 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.public')
+
+@section('content')
+    <h1>Hello World !</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProjectController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+Route::middleware(['auth'])->group(function () {
+    // Rotte per la gestione dei progetti
+    Route::resource('admin/projects', ProjectController::class);
+});
+
+require __DIR__ . '/auth.php';

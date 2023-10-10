0b1e463bc5e13b148cdc60e763858f9242414d66
PietroFiori91 <pederflowers@hotmail.it>
2023-10-10 17:08:13 +0200

    form request

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,8 +4,25 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
+
+use App\Http\Requests\ProjectCreateRequest;
+use App\Http\Requests\ProjectUpdateRequest;
 
 class ProjectController extends Controller
 {
-    //
+    public function store(ProjectCreateRequest $request)
+    {
+        $project = Project::create($request->validated());
+
+
+        return redirect()->route('admin.projects.index');
+    }
+
+    public function update(ProjectUpdateRequest $request, Project $project)
+    {
+        $project->update($request->validated());
+
+        return redirect()->route('admin.projects.index');
+    }
 }
